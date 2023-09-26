3bcc491eb08bed0934aee9dde6a5a688620285a5
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-26 02:19:58 -0700

    We now have a form that's posted

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const html = (strings, ...values) => String.raw({ raw: strings }, ...values);
+// body parser
+const bodyParser = require("body-parser");
+// handle form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/clicked", (_, res) => {
-  res.send(html`<p>you clicked me</p>`);
+app.get("/machine-list", (_, res) => {
+  const starterMachines = [
+    "Tricep Extension",
+    "Leg Extension",
+    "Chest Press",
+    "Lat Pulldown",
+    "Seated Rows",
+    "Leg Lift",
+    "Pec Fly",
+    "Pec Fly (Reverse)",
+    "Bicep Curl",
+    "Shoulder Press",
+    "Stairs",
+  ];
+  res.send(
+    html`${starterMachines.map((machine) => `<option>${machine}</option>`)}`,
+  );
+});
+
+app.post("/submit-lift", (req, res) => {
+  console.log(req.body);
+  res.send(`I will submit: ${JSON.stringify(req.body, null, 2)}`);
 });
 
 app.get("/", (_, res) => {
